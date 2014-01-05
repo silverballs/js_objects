35bc1434573afcd54db15cd00fe97f6a01e5ddb1
Silverballs <asilbersmith11@gmail.com>
2014-01-05 00:12:21 -0500

    Create objects

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // window.confirm("Am I working?")
+
+// We can create objects using literal or constructor notation
+	// literal:
+		var andrew = {
+			job: "developer", // string
+			isAwesome: true // boolean 
+			// can also hold objects as well as functions
+		};
+
+	// Construtor:  
+		//create a function that will allow instances of an object:
+		function Person(job,isAwesome) { // What should a person have?
+			this.job = job;
+			this.isAwesome = isAwesome;
+		}
+		//create new person:
+		var matt = new Person("sales", true); 
+
+
